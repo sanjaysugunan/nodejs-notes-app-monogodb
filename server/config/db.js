3c934828472252506dae64805d12446ced3c0612
s4njyy <s4njyy@gmail.com>
2023-07-01 21:23:17 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONDODB_URI);
+        console.log(`Database Connected ${conn.connection.host}`);
+    } catch (error) {
+         console.log(error);
+    }
+}
+
+module.exports = connectDB;
